710a8cf2ba78682bf62606dfb15fae631c817833
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2024-07-14 15:52:28 +0100

    display each image on cart

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,8 +1,7 @@
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import backIcon from "../assets/iconOutlined (2).png";
-import checkIcon from "../assets/iconOutlined.png";
-import boxIcon from "../assets/iconOutlined (1).png";
+
 import {
 	decreaseItemQuantity,
 	deleteItem,
@@ -13,7 +12,16 @@ import {
 const Cart = () => {
 	const dispatch = useDispatch();
 	const cartItems = useSelector((cart) => cart.cart.cart);
-	console.log(cartItems);
+
+	// Calculate subtotal for each item
+	const calculateSubtotal = (quantity, unitPrice) => quantity * unitPrice;
+
+	// Calculate total by summing up all the subtotals
+	const calculateTotal = (items) =>
+		items.reduce(
+			(total, item) => total + calculateSubtotal(item.quantity, item.unitPrice),
+			0
+		);
 
 	return (
 		<div className="flex flex-col lg:flex-row min-h-screen">
@@ -27,37 +35,45 @@ const Cart = () => {
 					{cartItems.map((item) => (
 						<div
 							key={item.id}
-							className="bg-white p-4 rounded-xl shadow-lg flex relative"
+							className="bg-white p-4 rounded-xl shadow-lg flex flex-col justify-between relative"
 						>
-							<div className="w-1/3 h-36 bg-gray-400 rounded-3xl mr-4 flex-shrink-0 flex items-center justify-center">
-								<img
-									src={item.img}
-									// src={`https://api.timbu.cloud/images/${item.photos.url}`}
-									alt={item.name}
-									className="w-full h-full object-cover rounded-3xl"
-								/>
-							</div>
-							<div className="flex-1 flex flex-col justify-between">
-								<div className="flex items-center mt-4">
-									<button
-										onClick={() => dispatch(decreaseItemQuantity(item.id))}
-										className="text-neutal-600 rounded-[84.65px] bg-zinc-300 p-2 shadow-md hover:bg-zinc-400 transition duration-300"
-									>
-										-
-									</button>
-
-									<p className="text-black text-xs px-2 font-bold font-['Outfit']">
-										{item.quantity}
+							<div className="flex">
+								<div className="w-[150px] h-[150px] flex-shrink-0 flex items-center justify-center mr-4">
+									<img
+										src={`https://api.timbu.cloud/images/${item.image}`}
+										alt={item.name}
+										className="w-full h-full object-contain rounded-lg"
+									/>
+								</div>
+								<div className="flex-1">
+									<h3 className="text-lg font-bold">{item.name.name}</h3>
+									<p className="text-gray-900 text-lg font-medium">
+										N{item.unitPrice}
 									</p>
-									<button
-										onClick={() => dispatch(increaseItemQuantity(item.id))}
-										className="bg-indigo-300 text-white font-medium text-base p-2 rounded-[84.75px] shadow-md hover:bg-indigo-400 transition duration-300"
-									>
-										+
-									</button>
+									<div className="flex items-center mt-4">
+										<button
+											onClick={() => dispatch(decreaseItemQuantity(item.id))}
+											className="text-neutral-600 rounded-[84.65px] bg-zinc-300 p-2 shadow-md hover:bg-zinc-400 transition duration-300"
+										>
+											-
+										</button>
+										<p className="text-black text-xs px-2 font-bold font-['Outfit']">
+											{item.quantity}
+										</p>
+										<button
+											onClick={() => dispatch(increaseItemQuantity(item.id))}
+											className="bg-indigo-300 text-white font-medium text-base p-2 rounded-[84.75px] shadow-md hover:bg-indigo-400 transition duration-300"
+										>
+											+
+										</button>
+									</div>
+									<p className="text-black text-lg font-extrabold font-['outfit'] leading-10 mt-2">
+										Sub Total: N
+										{calculateSubtotal(item.quantity, item.unitPrice)}
+									</p>
 								</div>
 							</div>
-							<div className="flex items-end">
+							<div className="mt-8 flex justify-end">
 								<button
 									onClick={() => dispatch(deleteItem(item.id))}
 									className="bg-red-400 text-white py-1 px-2 rounded"
@@ -68,60 +84,16 @@ const Cart = () => {
 						</div>
 					))}
 				</div>
-
-				<hr className="border-dashed border-2 border-gray-300 mt-24" />
-
-				<div className="flex flex-col space-y-4">
-					<div className="flex justify-between items-center">
-						<div className="flex gap-4">
-							<img src={checkIcon} alt="check icon" />
-							<p className="text-neutral-600 text-xl font-medium font-['outfit'] leading-10">
-								Sub Total:
-							</p>
-						</div>
-
-						<p className="text-black text-xl font-medium font-['outfit'] leading-10"></p>
-					</div>
-					<div className="flex justify-between items-center">
-						<div className="flex gap-4">
-							<img src={checkIcon} alt="check icon" />
-							<p className="text-neutral-600 text-xl font-medium font-['outfit'] leading-10">
-								VAT:
-							</p>
-						</div>
-
-						<p className="text-black text-xl font-medium font-['ourfit'] leading-10">
-							N999
-						</p>
-					</div>
-					<div className="flex justify-between items-center">
-						<div className="flex gap-4">
-							<img src={boxIcon} alt="check icon" />
-							<p className="text-neutral-600 text-xl font-medium font-['outfit'] leading-10">
-								Delivery:
-							</p>
-						</div>
-
-						<p className="text-neutral-400 text-xl font-bold font-['outfit'] leading-10">
-							N3000
-						</p>
-					</div>
-					<div className="flex justify-between items-center">
-						<p className="text-black text-2xl font-extrabold font-['outfit'] leading-10">
-							TOTAL:
-						</p>
-						<p className="text-black text-2xl font-extrabold font-['outfit'] leading-10">
-							N60,999
-						</p>
-					</div>
-					<div>
-						<button
-							onClick={() => dispatch(clearCart())}
-							className="bg-neutral-500 text-white text-xl py-4 px-8 rounded mt-8"
-						>
-							Clear
-						</button>
-					</div>
+				<div className="flex flex-col items-end mt-8">
+					<p className="text-black text-lg font-extrabold font-['outfit'] leading-10 mb-4">
+						Total: N{calculateTotal(cartItems)}
+					</p>
+					<button
+						onClick={() => dispatch(clearCart())}
+						className="bg-neutral-500 text-white text-xl py-4 px-8 rounded mt-2"
+					>
+						Clear
+					</button>
 				</div>
 			</div>
 
@@ -132,7 +104,7 @@ const Cart = () => {
 						Payment Info
 					</h3>
 					<div className="mb-4 mt-10">
-						<p className=" text-neutral-400 text-base font-normal font-['oufit'] leading-loose">
+						<p className="text-neutral-400 text-base font-normal font-['outfit'] leading-loose">
 							Choose Payment Method
 						</p>
 						<div className="flex flex-col">
@@ -154,7 +126,7 @@ const Cart = () => {
 						<p className="text-neutral-400 text-base font-normal font-['outfit'] leading-loose">
 							Card Holder Name
 						</p>
-						<hr className=" bg-zinc-300 mt-8" />
+						<hr className="bg-zinc-300 mt-8" />
 					</div>
 					<div className="mb-8 mt-4">
 						<p className="text-neutral-400 text-base font-normal font-['outfit'] leading-loose">
@@ -178,7 +150,7 @@ const Cart = () => {
 					</div>
 					<Link
 						to="/success"
-						className="text-white py-3 rounded-2xl bg-black text-center text-2xl font-['outfit] font-medium block w-full hover:opacity-1 transition duration-300"
+						className="text-white py-3 rounded-2xl bg-black text-center text-2xl font-['outfit'] font-medium block w-full hover:opacity-1 transition duration-300"
 					>
 						Checkout
 					</Link>
